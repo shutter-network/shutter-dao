5f3ddd944863b44751334b3dcc93dacd2e81c820
Daniel Dimitrov <daniel@compojoom.com>
2023-12-18 10:26:57 +0100

    fix: use SPT conversion address for token init

--- src/deploy/06_init_shutter_token.ts
+++ src/deploy/06_init_shutter_token.ts
@@ -19,8 +19,8 @@ const deployContracts = async function ({ deployments, config, network }) {
   const safeSalt = getDeploymentArguments<string>('SAFE_SALT', config, networkName);
 
   const predictedSafeAddress = await getPredictedSafeAddress(safeSalt);
-  // TODO: change this with the STP address once contract available
-  const sptContractAddress = predictedSafeAddress;
+  const sptContractDeployment = await deployments.get('SptConversion');
+  const sptContractAddress = sptContractDeployment.address;
   await shutterToken
     .connect(deployer)
     .initialize(predictedSafeAddress, sptContractAddress, airdrop.address);
